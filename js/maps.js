d6435f01db71423cdc8cdbfc0d3d29d55c08d002
ctroupin <charles.troupin@gmail.com>
2019-04-16 14:22:22 +0200

    map for EMODnet partners

--- js/maps.js
+++ js/maps.js
@@ -125,39 +125,188 @@ window.divaMap = (function() {
 				marker = new L.circleMarker([grid[i][0],grid[i][1]],
 					{color: "k", radius: 3, opacity: 1, fillOpacity:.9});
 					markerlist.push(marker);
-			};
-			var coarsegrid = L.layerGroup(markerlist);
+				};
+				var coarsegrid = L.layerGroup(markerlist);
 				// coarsegrid.addTo(map);
 
-			markerlist2 = []
-			for (var i = 0; i < grid2.length; i++) {
-				marker = new L.circleMarker([grid2[i][0], grid2[i][1]],
-					{color: "k", radius: 2, opacity: 1, fillOpacity:.9});
-					markerlist2.push(marker);
-			};
-			var finegrid = L.layerGroup(markerlist2);
+				markerlist2 = []
+				for (var i = 0; i < grid2.length; i++) {
+					marker = new L.circleMarker([grid2[i][0], grid2[i][1]],
+						{color: "k", radius: 2, opacity: 1, fillOpacity:.9});
+						markerlist2.push(marker);
+					};
+					var finegrid = L.layerGroup(markerlist2);
 
 
-			var overlayers = {
-				"Cities": cities,
-				"Heat map": heatmap,
-				"Coarse grid": coarsegrid,
-				"Fine grid": finegrid,
-				"Bathymetry": bathymetry,
-				"Contours": divacontours,
-				"Finite-element mesh": divamesh,
-				"Interpolated field": divafield,
-			};
+					var overlayers = {
+						"Cities": cities,
+						"Heat map": heatmap,
+						"Coarse grid": coarsegrid,
+						"Fine grid": finegrid,
+						"Bathymetry": bathymetry,
+						"Contours": divacontours,
+						"Finite-element mesh": divamesh,
+						"Interpolated field": divafield,
+					};
 
-			map.setView([38.5,10], 5);
-			L.control.scale().addTo(map);
+					map.setView([38.5,10], 5);
+					L.control.scale().addTo(map);
 
-			L.control.layers(baseMaps, overlayers,
-				{autoZIndex:true, collapsed:true}).addTo(map);
+					L.control.layers(baseMaps, overlayers,
+						{autoZIndex:true, collapsed:true}).addTo(map);
 
-			},
-			stop: function() {
-				map.remove();
-			}
-		};
-	})();
+					},
+					stop: function() {
+						map.remove();
+					}
+				};
+			})();
+
+
+			window.emodnetMap = (function() {
+				var map;
+
+				return {
+					start: function() {
+						var map = L.map('emodnet-map', {
+							fullscreenControl: true,
+						}).setView([48., 10], 4);
+
+						map.createPane('labels');
+						map.getPane('labels').style.zIndex = 650;
+						map.getPane('labels').style.pointerEvents = 'none';
+
+						var Esri_OceanBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
+							attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
+							maxZoom: 13
+						}).addTo(map);
+
+						// control that shows state info on hover
+						var info = L.control();
+
+						info.onAdd = function (map) {
+							this._div = L.DomUtil.create('div', 'info');
+							this.update();
+							return this._div;
+						};
+
+						info.update = function (props) {
+							if (typeof(props) !== 'undefined') {
+								if (props.npartners == null) {
+									this._div.innerHTML = '<h4>' + props.NAME + '</h4>  no partners'
+								} else
+								{
+									this._div.innerHTML = '<h4>' + props.NAME + '</h4> ' + props.npartners + ' partners'
+								};
+							}
+							else {
+								this._div.innerHTML = '<h4> Select a country </h4>';
+							};
+						};
+
+						info.addTo(map);
+
+						var geojson;
+
+						function getColor(d) {
+							return d > 7 ? '#bd0026' :
+							d > 5  ? '#f03b20' :
+							d > 2  ? '#fd8d3c' :
+							d > 1  ? '#fecc5c' :
+							d > 0  ? '#ffffb2' :
+							'#E8E1E1';
+
+						}
+
+
+						function style(feature) {
+							return {
+								fillColor: getColor(feature.properties.npartners),
+								weight: .5,
+								opacity: 1,
+								color: 'black',
+								dashArray: '0',
+								fillOpacity: 0.6
+							};
+						}
+
+						function highlightFeature(e) {
+							var layer = e.target;
+
+							layer.setStyle({
+								weight: 5,
+								color: '#666',
+								dashArray: '',
+								fillOpacity: 0.2
+							});
+
+							if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+								layer.bringToFront();
+							}
+
+							info.update(layer.feature.properties);
+						}
+
+						function resetHighlight(e) {
+							geojson.resetStyle(e.target);
+							info.update();
+						}
+
+						function zoomToFeature(e) {
+							map.fitBounds(e.target.getBounds());
+						}
+
+
+						function onEachFeature(feature, layer) {
+							layer.on({
+								mouseover: highlightFeature,
+								mouseout: resetHighlight,
+								click: zoomToFeature
+							});
+						}
+
+						geojson = L.geoJson(countries, {
+							style: style,
+							onEachFeature: onEachFeature
+						}).addTo(map);
+
+
+						var legend = L.control({position: 'bottomright'});
+						legend.onAdd = function (map) {
+
+							var div = L.DomUtil.create('div', 'info legend'),
+							grades = [0, 1, 2, 5, 7],
+							labels = [],
+							from, to;
+
+							//for (var i = 0; i < grades.length; i++) {
+							for (var i = grades.length-1; i >= 0; i--) {
+								from = grades[i];
+								to = grades[i + 1];
+
+								if (to - from == 1){
+									labels.push(
+										'<i style="background:' + getColor(from + 1) + '"></i> ' + to );
+									} else {
+										lower = from + 1;
+										labels.push(
+											'<i style="background:' + getColor(from + 1) + '"></i> ' +
+											lower + (to ? '&ndash;' + to : ' and more'));
+										}
+									}
+									div.innerHTML = labels.join('<br>');
+									return div;
+								};
+
+								legend.addTo(map);
+
+								L.marker([51.233318, 2.929550]).addTo(map);
+								L.marker([50.836687, 4.376782]).addTo(map);
+
+
+							},
+							stop: function() {
+								map.remove();
+							}
+						};
+					})();
