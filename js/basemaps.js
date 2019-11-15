f0372aac12efe5b672c68b9d9dd0c2ec6d229a74
ctroupin <charles.troupin@gmail.com>
2019-11-15 09:55:58 +0100

    new basemap

--- js/basemaps.js
+++ js/basemaps.js
@@ -61,3 +61,9 @@ var CartoDB_PositronNoLabels = L.tileLayer('https://cartodb-basemaps-{s}.global.
 	subdomains: 'abcd',
 	maxZoom: 19
 });
+
+var CartoDB_DarkMatterNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
